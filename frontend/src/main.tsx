96027920ae09aa9e6e4ec9e5a60c351e00d8e1e6
Ziad Ahmed <ziadahmedcf@gmail.com>
2024-06-28 03:43:05 +0300

    init

---
+++ frontend/src/main.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainLayout from "./layouts/MainLayout/MainLayout";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<MainLayout />);
